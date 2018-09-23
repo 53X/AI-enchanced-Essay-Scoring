41034359193156dfc95a1ceebd9eba70cd692a05
53X <mirage_1997@mail.ru>
2018-09-23 23:05:22 +0530

    model made

--- model.py
+++ model.py
@@ -1,5 +1,12 @@
 import keras
-from keras.layers import Embedding, Conv1D, Input, Reshape, Dropout, LSTM
+from keras.layers import Embedding, Conv1D, Input, Reshape, Dropout, LSTM, Lambda
+from keras import backend as K
+from keras.models import Model
+
+
+def mean_over_time(input_tensor):
+
+	return K.mean(input_tensor, axis=1)
 
 
 def essay_model(maxlen, vocab_size):
@@ -14,7 +21,27 @@ def essay_model(maxlen, vocab_size):
 	conv_layer = Conv1D(filters=1, kernel_size=3, strides=1, padding='same',
 						name='conv_layer', data_format='channels_first', activation='relu')(reshape)
 
-	recurrent_layer = LSTM()
+	recurrent_layer = LSTM(units=300, dropout=0.5, recurrent_dropout=0.5, return_sequences=True)(conv_layer)
+
+	mylayer = Lambda(mean_over_time, output_shape=(300, ))(recurrent_layer)
+
+	dropout = Dropout(0.5)(mylayer)
+
+	output = Dense(1, activation='sigmoid')(dropout)
+
+	model = Model(inputs=input_layer, outputs=output)
+
+	model.compile(loss = 'binary_crossentropy', metrics=['binary_accuracy'])
+
+	return model
+
+
+	
+
+
+
+
+
 
 						
 
